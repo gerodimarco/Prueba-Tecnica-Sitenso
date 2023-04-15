42e3d3b3887ae3f65aec78630ec802370e60fb6a
Geronimo Dimarco <unzagageronimo@gmail.com>
2023-04-15 14:39:43 -0300

    Navbar personalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
+
+// https://gateway.marvel.com:443/v1/public/characters?apikey=4c29828e2fe3a5ef253cf528691d77b1
+
+// key publica: 4c29828e2fe3a5ef253cf528691d77b1
+// key privada: 63bb2ad248451dec656324da9f6e05baba18d0aa
+// ts:1
+// md5:  163bb2ad248451dec656324da9f6e05baba18d0aa4c29828e2fe3a5ef253cf528691d77b1
+// hash: a8bade49ffcaec6a4e682725e6f5fe86
 
 function App() {
+  const [personajes, setPersonajes]=useState([])
+
+
+  useEffect(()=> {
+    axios.get('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=4c29828e2fe3a5ef253cf528691d77b1&hash=a8bade49ffcaec6a4e682725e6f5fe86')
+    .then(res=>{setPersonajes(res.data.data.results)
+    }).catch(error=>console.log(error))
+  },[])
+
+  console.log(personajes)
+
+
+
+
+
   return (
-    <div className="App">
-      <h1>Marvel</h1>
-    </div>
-  );
+    
+
+    <Navbar />
+
+
+    
+  )
+
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function BasicExample() {
+  return (
+    <Navbar variant="dark" bg="danger" expand="lg" >
+      <Container>
+        <Navbar.Brand href="#home">MARVEL</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto text-white">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Personajes</Nav.Link>
+            <NavDropdown title="Comics" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Spiderman</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Dr.Strange</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Iron Man</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
+
